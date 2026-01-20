1dc69571ffc48f120dad65807d2cf826d4afdda6
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-01-20 12:25:35 +0100

    task-1 done

--- task-1/leap-year.js
+++ task-1/leap-year.js
@@ -1,9 +1,20 @@
-import promptSync from 'prompt-sync';
+import promptSync from "prompt-sync";
 const prompt = promptSync();
 
+const year = Number(prompt("Enter a year to check if it is a leap year: "));
 
-// Write your code here
-// Guidance:
-// Step 1: prompt the user to enter a year
-// Step 2: convert the user input to a number so we can perform calculations
-// Step 3: Implement the logic
+if (year >= 1 && year <= 9999) {
+  if (!(year % 4)) {
+    if (!(year % 100) && !(year % 400)) {
+      console.log(`Yes, ${year} is a leap year`);
+    } else if (year % 100) {
+      console.log(`Yes, ${year} is a leap year`);
+    } else {
+      console.log(`No, ${year} is not a leap year`);
+    }
+  } else {
+    console.log(`No, ${year} is not a leap year`);
+  }
+} else {
+  console.log("Invalid year! Enter year from 1 till 9999");
+}
