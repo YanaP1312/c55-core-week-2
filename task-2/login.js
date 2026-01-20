d220ec15678ee2efea11c029f6a5f15ec4c6438b
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-01-20 14:40:30 +0100

    fix converter.js

--- task-2/login.js
+++ task-2/login.js
@@ -1,24 +1,28 @@
 // Do not change the line below
-import { errorMessage, successMessage } from './app.js';
+import { errorMessage, successMessage } from "./app.js";
 
 let incorrectAttempts = 0;
 
 function onLogin(username, password) {
+  const isValid =
+    (username === "admin" && password === "Hack1234") ||
+    (username === "user" && password === "7654321");
 
-  const isValid = (username === "admin" && password === "Hack1234") || (username === "user" && password === "7654321");
-
-  if(incorrectAttempts > 3) {
-    errorMessage('Login blocked: Too many incorrect attempts');
-    return
-  } else if (isValid){
+  if (incorrectAttempts > 3) {
+    errorMessage("Login blocked: Too many incorrect attempts");
+    return;
+  } else if (isValid) {
     incorrectAttempts = 0;
-    successMessage('Logged in successfully');
+    successMessage("Logged in successfully");
   } else {
     incorrectAttempts++;
-    if(incorrectAttempts === 4){errorMessage('Login blocked: Too many incorrect attempts');} else {
-    errorMessage('Incorrect credentials')}}
+    if (incorrectAttempts === 4) {
+      errorMessage("Login blocked: Too many incorrect attempts");
+    } else {
+      errorMessage("Incorrect credentials");
+    }
   }
-
+}
 
 // Do not change the line below
 export { onLogin };
