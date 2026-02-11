55c9ac9f9c70b20f5d13e712f60a70561fb80ba1
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-02-11 21:35:22 +0100

    add check for isNan and isInteger

--- task-1/leap-year.js
+++ task-1/leap-year.js
@@ -3,10 +3,14 @@ const prompt = promptSync();
 
 const year = Number(prompt("Enter a year to check if it is a leap year: "));
 
-if (year < 1 || year > 9999) {
-    console.log("Invalid year!");
-} else if (!(year % 400) || (!(year % 4) && (year % 100))) {
-    console.log(`Yes, ${year} is a leap year`);
+if (Number.isNaN(year)) {
+  console.log("Please, enter a number");
+} else if (!Number.isInteger(year)) {
+  console.log("Please, enter a whole number (no decimals)");
+} else if (year < 1 || year > 9999) {
+  console.log("Invalid year!");
+} else if (!(year % 400) || (!(year % 4) && year % 100)) {
+  console.log(`Yes, ${year} is a leap year`);
 } else {
-    console.log(`No, ${year} is not a leap year`);
+  console.log(`No, ${year} is not a leap year`);
 }
