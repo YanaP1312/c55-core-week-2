54020febbf58a3ef07d77d26e1bdc65e2b562ba2
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-02-11 21:43:13 +0100

    extract leap year logic into isLeapYear variable

--- task-1/leap-year.js
+++ task-1/leap-year.js
@@ -3,13 +3,15 @@ const prompt = promptSync();
 
 const year = Number(prompt("Enter a year to check if it is a leap year: "));
 
+const isLeapYear = year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+
 if (Number.isNaN(year)) {
   console.log("Please, enter a number");
 } else if (!Number.isInteger(year)) {
   console.log("Please, enter a whole number (no decimals)");
 } else if (year < 1 || year > 9999) {
   console.log("Invalid year!");
-} else if (!(year % 400) || (!(year % 4) && year % 100)) {
+} else if (isLeapYear) {
   console.log(`Yes, ${year} is a leap year`);
 } else {
   console.log(`No, ${year} is not a leap year`);
